121ca76a0503cbeda1c627347b122c17998b8dd4
gonza <gonzalososa342@gmail.com>
2025-08-06 00:51:10 -0300

    Uso de agent mode con instrucciones de MUI

--- reactapptest.client/src/App.tsx
+++ reactapptest.client/src/App.tsx
@@ -1,4 +1,16 @@
 import { useEffect, useState } from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import CircularProgress from '@mui/material/CircularProgress';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
 import './App.css';
 
 interface Forecast {
@@ -16,34 +28,48 @@ function App() {
     }, []);
 
     const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tableLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
+        ? <Box display="flex" flexDirection="column" alignItems="center" mt={4}>
+            <CircularProgress />
+            <Typography variant="body1" mt={2} sx={{ textWrap: "balance" }}>
+                Loading... Please refresh once the ASP.NET backend has started. See{' '}
+                <Link href="https://aka.ms/jspsintegrationreact" target="_blank" rel="noopener">
+                    https://aka.ms/jspsintegrationreact
+                </Link>{' '}for more details.
+            </Typography>
+        </Box>
+        : <TableContainer component={Paper}>
+            <Table aria-label="Weather forecast table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Date</TableCell>
+                        <TableCell>Temp. (C)</TableCell>
+                        <TableCell>Temp. (F)</TableCell>
+                        <TableCell>Summary</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {forecasts.map(forecast => (
+                        <TableRow key={forecast.date}>
+                            <TableCell>{forecast.date}</TableCell>
+                            <TableCell>{forecast.temperatureC}</TableCell>
+                            <TableCell>{forecast.temperatureF}</TableCell>
+                            <TableCell>{forecast.summary}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>;
 
     return (
-        <div>
-            <h1 id="tableLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
+        <Container maxWidth="md">
+            <Typography variant="h4" component="h1" gutterBottom id="tableLabel">
+                Weather forecast
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+                This component demonstrates fetching data from the server.
+            </Typography>
             {contents}
-        </div>
+        </Container>
     );
 
     async function populateWeatherData() {
