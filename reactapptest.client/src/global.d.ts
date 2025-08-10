519b8e859f594dd8a9d853da0c8774e6c232db65
gonza <gonzalososa342@gmail.com>
2025-08-10 18:36:56 -0300

    Añadir declaraciones de módulos y variables de entorno
    Se han añadido declaraciones de módulos para archivos CSS, SVG, imágenes (PNG, JPG, JPEG, GIF, BMP, TIFF) y JSON, permitiendo que TypeScript los reconozca. También se han definido interfaces para las variables de entorno personalizadas para facilitar su acceso en el desarrollo.

---
+++ reactapptest.client/src/global.d.ts
@@ -0,0 +1,59 @@
+declare module '*.css' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module '*.module.css';
+
+declare module '*.svg' {
+  import * as React from 'react';
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.gif' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.bmp' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.tiff' {
+  const value: string;
+  export default value;
+}
+
+// Permite importar archivos JSON como módulos
+declare module '*.json' {
+  const value: unknown;
+  export default value;
+}
+
+// Declaraciones para variables de entorno personalizadas
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  // Añade aquí otras variables de entorno si es necesario
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
