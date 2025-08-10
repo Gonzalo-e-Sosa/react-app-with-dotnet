705d7f9492efee7db35d9f3474b9f3ac95ce2677
gonza <gonzalososa342@gmail.com>
2025-08-10 18:41:08 -0300

    Config de vitest para proyecto

---
+++ reactapptest.client/vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [react()],
+    test: {
+        include: ['**/__tests__/**/*.test.{js,jsx,ts,tsx}'],
+        environment: 'jsdom',
+        globals: true,
+        setupFiles: './__tests__/setupTests.ts',
+        coverage: {
+            provider: 'v8',
+            reporter: ['text','json', 'html'],
+        },
+    },
+});
