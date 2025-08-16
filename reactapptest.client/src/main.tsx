769592428b141a95efd5d457daaade6240a61d32
gonza <gonzalososa342@gmail.com>
2025-08-15 23:54:28 -0300

    feat: implement dark and light theme support with MUI

--- reactapptest.client/src/main.tsx
+++ reactapptest.client/src/main.tsx
@@ -1,14 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import lightTheme from './themes/light.ts';
+import App from './App.tsx'
 import './index.css'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
