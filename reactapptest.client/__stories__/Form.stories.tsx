0f77d22303f9649d93b73b9340679eb19e7127f1
gonza <gonzalososa342@gmail.com>
2025-08-17 18:43:47 -0300

    Reubicar archivos de stories

---
+++ reactapptest.client/__stories__/Form.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+import Form from '@/components/form';
+
+const meta = {
+    title: 'Components/Form',
+    component: Form.Form,
+    decorators: [
+        (Story) => (
+            <Form.Provider>
+                <Story />
+            </Form.Provider>
+        )
+    ]
+} satisfies Meta<typeof Form.Form>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        children: (
+            <>
+                <input type="text" placeholder="Name" />
+                <input type="email" placeholder="Email" />
+            </>
+        )
+    }
+};
