173a404d389fd0372f7c768fe2ee89be447715c7
gonza <gonzalososa342@gmail.com>
2025-08-17 22:59:16 -0300

    feat: add Cliente class and IClienteRepository interface for client management

---
+++ ReactAppTest.Server/Domain/Fiscal/IClienteRepository.cs
@@ -0,0 +1,10 @@
+namespace ReactAppTest.Server.Domain.Fiscal;
+
+public interface IClienteRepository
+{
+    Task<Cliente?> GetByIdAsync(int id);
+    Task<IEnumerable<Cliente>> GetAllAsync();
+    Task AddAsync(Cliente cliente);
+    Task UpdateAsync(Cliente cliente);
+    Task DeleteAsync(int id);
+}
